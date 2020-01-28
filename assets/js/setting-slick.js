d54bfac75ef8ea49ad5850fab20b2e0e4909d0de
patricia <yively.pa@gmail.com>
2020-01-28 14:42:53 -0400

    separando secciones

---
+++ assets/js/setting-slick.js
@@ -0,0 +1,34 @@
+
+$('.main-clients__content').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  dots: false,
+  arrows: true,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+   
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
